260c8c17938c0d6af79473b4ca579c84dee2d8f9
= <=>
2026-02-27 11:28:59 +0545

    Switched to Crawl4ai due to limitations of native webbaseloader

--- intermediate_wrapper.py
+++ intermediate_wrapper.py
@@ -6,7 +6,7 @@ from langchain_core.prompts import PromptTemplate
 from dotenv import load_dotenv
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin, urlparse
-
+from crawl4ai import AsyncWebCrawler
 import os
 os.environ["USER_AGENT"] = "Mozilla/5.0 (X11; Linux x86_64)"
 
@@ -14,82 +14,66 @@ load_dotenv(dotenv_path=".env")
 
 base_url = "https://www.saflora.com.np"
 
-loader = WebBaseLoader(base_url)
-docs = loader.load()
-scraped_html = loader._scrape(base_url)
+class Parser():
+    def parse_links(self, html):
+        parsed = BeautifulSoup(html, "html.parser")
+        return [link["href"] for link in parsed.find_all("a", href=True)]
 
-print(docs)
-print(scraped_html)
 
-html = docs[0].page_content
-soup = BeautifulSoup(html, "html.parser")
+class Crawler():
+    async def crawl(self, url):
+        async with AsyncWebCrawler() as crawler:
+            res = await crawler.arun(url=url)
+            return res
+    
+class Splitter():
+    def __init__(self):
+        splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000,
+            chunk_overlap=100,
+            separators=["\n\n", "\n", " ", ""],
+            length_function=len
+        )
+        self.splitter = splitter
+    def split_text(self, text):
+       res = self.splitter.split_text(text)
+       return res
 
-links = []
+class SafloraWrapper():
+    def __init__(self, stream = False):
+        self.stream = stream
+        self.llm = ChatGroq(
+            model="llama-3.1-8b-instant",
+            temperature=0.7,
+            streaming=self.stream
+        )
+        self.system = SystemMessage(content="You are a professional ai assistant for Saflora.Your domain is Saflora only if asked about any other subjects you will politely refuse to answer. You will only answer the questions related to Saflora. If the question is not related to Saflora, you will politely refuse to answer.")
+        self.prompt = PromptTemplate.from_template("question:{prompt}")
 
-for a in soup.find_all("a", href=True):
-    full_url = urljoin(base_url, a["href"])
-    print(full_url)
-    if urlparse(full_url).netloc == urlparse(base_url).netloc:
-        links.append(full_url)
+        self.history = [self.system]
 
-# Remove homepage duplicate
-print(links)
+    def ask(self, message):
+        formatted = self.prompt.format(
+            prompt=message,
+        )
+        user_message = HumanMessage(content=formatted)
 
-# Load all discovered pages
-multi_loader = WebBaseLoader(links)
-all_docs = multi_loader.load()
-print(f"Loaded {len(all_docs)}: {all_docs} documents")
+        self.history.append(user_message)
+        response = self.llm.invoke(self.history)
+        self.history.append(response)
 
-# splitter = RecursiveCharacterTextSplitter(
-#     chunk_size=300,
-#     chunk_overlap=50,
-# )
-
-# # print(f"formal text {res}")
-
-# html_content = res[0].page_content
-# print(f"HTML: {html_content[:2000]}")
-# splitted_res = splitter.split_text(html_content)
-
-# print(f"Spillted Text {splitted_res}")
-
-# class SafloraWrapper():
-#     def __init__(self, stream = False):
-#         self.stream = stream
-#         self.llm = ChatGroq(
-#             model="llama-3.1-8b-instant",
-#             temperature=0.7,
-#             streaming=self.stream
-#         )
-#         self.system = SystemMessage(content="You are a professional ai assistant for Saflora.Your domain is Saflora only if asked about any other subjects you will politely refuse to answer. You will only answer the questions related to Saflora. If the question is not related to Saflora, you will politely refuse to answer.")
-#         self.prompt = PromptTemplate.from_template("question:{prompt}")
-
-#         self.history = [self.system]
-
-#     def ask(self, message):
-#         formatted = self.prompt.format(
-#             prompt=message,
-#         )
-#         user_message = HumanMessage(content=formatted)
-
-#         self.history.append(user_message)
-#         response = self.llm.invoke(self.history)
-#         self.history.append(response)
-
-#         return response.content
+        return response.content
     
 
-# if __name__ == "__main__":
-#     wrapper = SafloraWrapper(
-#         stream=False
-#     )
-#     result1 = wrapper.ask(
-#         message="What is Saflora?"
-#     )
+if __name__ == "__main__":
+    import asyncio
 
-#     result2 = wrapper.ask(
-#         message="Who are the founders?"
-#     )
-
-#     print(result1, "\n\n", result2)
+    async def main():
+        crawler = Crawler()
+        res = await crawler.crawl("https://www.saflora.com.np")
+        print(res.html)
+        parser = Parser()
+        parsed_res = parser.parse_links(res.html)
+        print(parsed_res)
     
+    asyncio.run(main())
